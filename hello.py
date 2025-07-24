c1c444aa951b969aeec0a7c6aaffd5f46358ebaa
JuniorSarh <sabelohgumede@gmail.com>
2025-07-24 11:35:55 +0200

    next commit

---
+++ hello.py
@@ -0,0 +1,6 @@
+#first Python script
+
+First_name=input("Please Enter your First name").strip().title()
+LastName=input("Please Enter Your Last Name").strip().title()
+
+print(f"\n hello, {First_name} {LastName}. It is Pleasure to meet you!")
